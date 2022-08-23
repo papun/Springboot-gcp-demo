13613873ba7f28efee5cd1463fae74f64253583a
papun <papun.sahoo@gmail.com>
2022-08-23 14:43:52 +0530

    Update HelloworldApplication.java

--- src/main/java/com/example/hw/HelloworldApplication.java
+++ src/main/java/com/example/hw/HelloworldApplication.java
@@ -17,7 +17,7 @@ public class HelloworldApplication {
 
         @GetMapping("/hello")
         public String hello(){
-            return "HelloWorld";
+            return "HelloWorld123";
         }
     }
 
